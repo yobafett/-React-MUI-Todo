c6f43c6d073002ef7c9ebe672b8381ba570dc64e
Yoba Fett <alexka.680@gmail.com>
2022-11-07 08:02:55 +0900

    Add top bar component

---
+++ src/components/TopBar.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import { TaskAlt } from '@mui/icons-material';
+
+const TopBar = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton color="inherit" sx={{ mr: 2 }}>
+                    <TaskAlt />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Todo List
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default TopBar;
